c4c19e073f8acaf569ad82777167f41b2b6d5dea
hopeaktian <hopeaktian@gmail.com>
2019-03-28 21:34:09 +0800

    change dirname

--- Nginx—_install/salt-master-cmd.sh
+++
@@ -1,3 +0,0 @@
-[root@pro1 ~]# salt-cp 'pro5' /root/nginx-install.sh /root/nginx-install.sh 
-[root@pro1 ~]# salt-cp 'pro5' /root/nginx-1.14.2.tar.gz /usr/local/src/nginx-1.14.2.tar.gz
-[root@pro1 ~]# salt 'pro5' cmd.run "sh /root/nginx-install.sh"
